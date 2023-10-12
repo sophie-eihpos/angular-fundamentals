e42d31d3ab1283d3bbb2cc92dbeac5456ae9b344
Sophie Jiang <sifeij@gmail.com>
2023-10-12 12:19:01 -0700

    add site header

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,26 +1,33 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'event-thumbnail',
-    template: `
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <!-- escape the dollar sign -->
-            <div>price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
+  selector: 'event-thumbnail',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{ event.name }}</h2>
+      <div>Date: {{ event.date }}</div>
+      <div>Time: {{ event.time }}</div>
+      <!-- escape the dollar sign -->
+      <div>price: \${{ event.price }}</div>
+      <div>
+        <span>Location: {{ event.location.address }}</span>
+        <span class="pad-left"
+          >{{ event.location.city }}, {{ event.location.country }}</span
+        >
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
     `,
-    styles: [`
-        .pad-left { margin-left: 10px; }
-        .well div { color: #bbb; }
-    `]
+  ],
 })
 export class EventThumbnailComponent {
-    @Input() event:any;
-    
+  @Input() event: any;
 }
