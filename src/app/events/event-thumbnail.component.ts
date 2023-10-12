d94a338d2907f069b98fba8e2171b8dc450c4b06
Sophie Jiang <sifeij@gmail.com>
2023-10-12 10:01:27 -0700

    Use template variables to interact with child component

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -14,18 +14,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
                 <span>&nbsp;</span>
                 <span>{{event.location.city}}, {{event.location.country}}</span>
             </div>
-            <button class="btn btn-primary" (click)="handleClickMe()">
-                Click me!
-            </button>
         </div>
     `
 })
 export class EventThumbnailComponent {
-    @Input() event: any;
-    @Output() eventClick = new EventEmitter();
+    @Input() event:any;
+    someProperty:string = "some string value to pass from child component to parent component";
 
-    handleClickMe() {
-        // console.log('clicked!');
-        this.eventClick.emit(this.event.name);
+    logEventName() {
+        console.log(this.event.name);
     }
+
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,13 @@ import { Component } from "@angular/core";
     <div>
         <h1>Upcoming Angular Events</h1>
         <hr>
-        <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+
+        <h3>{{thumbnail.someProperty}}</h3>
+        
+        <button class="btn btn-primary" (click)="thumbnail.logEventName()">
+            Log me the event name
+        </button>
     </div>
     `
 })
@@ -25,7 +31,4 @@ export class EventsListComponent {
         }
     };
 
-    handleEventClicked(data: any) {
-        console.log('received: ', data);
-    }
 }
