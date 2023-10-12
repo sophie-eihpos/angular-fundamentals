5fa41642c36c00b739089b60aa554a468e2c0df1
Sophie Jiang <sifeij@gmail.com>
2023-10-12 16:25:08 -0700

    repeat thumbnails with ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -19,6 +19,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   `,
   styles: [
     `
+      .thumbnail {
+        min-height: 210px;
+      }
       .pad-left {
         margin-left: 10px;
       }
