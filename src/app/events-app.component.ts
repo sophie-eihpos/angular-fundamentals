44d0cf180619e94d3d15959b73199065e08ed944
Sophie Jiang <sifeij@gmail.com>
2023-10-12 08:38:09 -0700

    component with template HTML with data-bound

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app-root',
   template: `
-    <h2>Hello World</h2>
+    <events-list></events-list>
     <img src="/assets/images/basic-shield.png" />
   `
 })
