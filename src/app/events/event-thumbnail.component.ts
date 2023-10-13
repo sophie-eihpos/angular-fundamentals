176599ff11aaa07c3137baa1c09a409b7f03f60a
Sophie Jiang <sifeij@gmail.com>
2023-10-12 17:30:26 -0700

    apply one style dynamically

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     <div class="well hoverwell thumbnail" *ngIf="event">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div [ngClass]="getStartTimeClass()"
+      <div [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'"
         [ngSwitch]="event?.time">
         Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
