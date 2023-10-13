b6fdfccfe3907676a68a44d0c9503471ed963458
Sophie Jiang <sifeij@gmail.com>
2023-10-12 19:06:47 -0700

    create event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   imports: [BrowserModule],
@@ -14,6 +15,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent,
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent],
 })
 export class AppModule {}
