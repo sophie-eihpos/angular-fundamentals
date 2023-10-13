8978b7ad181411c7e26b269a3872df20bd076634
Sophie Jiang <sifeij@gmail.com>
2023-10-12 17:04:09 -0700

    hide and show elements using ngIf

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,17 +3,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
+    <div class="well hoverwell thumbnail" *ngIf="event">
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div>Time: {{ event?.time }}</div>
       <!-- escape the dollar sign -->
-      <div>price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
-        <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
-        >
+      <div>price: \${{ event?.price }}</div>
+      <div *ngIf="event.location">
+        <span>Location: {{ event?.location.address }}</span>
+        <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
+      </div>
+      <div *ngIf="event.onlineUrl">
+        Online URL: {{ event?.onlineUrl }}
       </div>
     </div>
   `,
