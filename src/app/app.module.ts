9857796eaa3af51055cea6846d87da66ec525fb4
Sophie Jiang <sifeij@gmail.com>
2023-10-13 16:55:50 -0700

    organize exports with barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { RouterModule } from '@angular/router'; // cannot put it before BrowerMo
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,14 @@
 import { Routes, mapToCanActivate } from "@angular/router";
 
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { CreateEventComponent } from "./events/create-event.component";
+import {
+    EventDetailsComponent,
+    EventsListComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index';
+
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventListResolver } from "./events/events-list-resolver.service";
 
 export const appRoutes: Routes = [
     { 
