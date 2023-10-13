f11253294ffade9c6ecf11136984f112c49de8c4
Sophie Jiang <sifeij@gmail.com>
2023-10-12 17:20:38 -0700

    dynamically display two classes for styling

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     <div class="well hoverwell thumbnail" *ngIf="event">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div [class.green]="event?.time === '8:00 am'"
+      <div [ngClass]="{ green: event?.time === '8:00 am', bold: event?.time === '8:00 am' }"
         [ngSwitch]="event?.time">
         Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
@@ -26,6 +26,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   `,
   styles: [`
       .green { color: #003300 !important; }
+      .bold { font-weight: bold; }
       .thumbnail { min-height: 210px; }
       .pad-left { margin-left: 10px; }
       .well div { color: #bbb; }
