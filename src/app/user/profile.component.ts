1341d605383efccbf611bb4f8089cfedabc02e4f
Sophie Jiang <sifeij@gmail.com>
2023-10-15 10:46:32 -0700

    add multiple validators for first name on profile page

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -24,7 +24,9 @@ export class ProfileComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.firstName = new FormControl(this.authService.currentUser?.firstName, Validators.required);
+        this.firstName = new FormControl(
+            this.authService.currentUser?.firstName, 
+            [Validators.required, Validators.pattern('[a-zA-Z].*')]);
         this.lastName = new FormControl(this.authService.currentUser?.lastName, Validators.required);
 
         this.profileForm = new FormGroup({
