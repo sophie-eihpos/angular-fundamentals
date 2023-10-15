01653d454e7d20a9e1b6e921c4e75e2de2cdb1f3
Sophie Jiang <sifeij@gmail.com>
2023-10-14 19:05:27 -0700

    navigate to events page after login or cancel login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html'
@@ -8,10 +9,16 @@ export class LoginComponent {
   userName: string;
   password: string;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+    private router: Router) {
   }
 
   login(formValues: any) {
     this.authService.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
