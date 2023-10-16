3f28d16a8309ed1ad573c70427786b161280dc6f
Sophie Jiang <sifeij@gmail.com>
2023-10-16 16:27:03 -0700

    use multiple slot content project with ng-content
    select attribute is recommended

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,13 +4,17 @@ import { Component, Input } from "@angular/core";
     selector: 'collapsible-well',
     template: `
     <div (click)="toggleContent()" class="well pointable">
-        <h4 class="well-title">{{title}}</h4>
-        <ng-content *ngIf="visible"></ng-content>
+        <h4>
+            <ng-content select="[well-title]"></ng-content>
+        </h4>
+        <ng-content *ngIf="visible" select="[well-body]"></ng-content>
     </div>
     `
 })
 export class CollapsibleWellComponent {
-    @Input() title: string;
+    // no need anymore since we are using ng-content for content projection now
+    // @Input() title: string;
+
     visible: boolean = true;
 
     toggleContent() {
