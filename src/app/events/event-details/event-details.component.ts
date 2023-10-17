5da33d609c902c946545d9b3beab99710f6f7e80
Sophie Jiang <sifeij@gmail.com>
2023-10-16 18:47:09 -0700

    create filter buttons on event detail page

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -15,6 +15,7 @@ import { ISession } from "../shared";
 export class EventDetailsComponent implements OnInit {
     event: any = {};
     addMode: boolean = false;
+    filterBy: string = 'all';
 
     constructor(private eventService: EventService,
         private activatedRoute: ActivatedRoute) {        
