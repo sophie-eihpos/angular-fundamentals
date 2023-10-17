37057eee7ef73909ddd77dc47c4555f17f11b5e4
Sophie Jiang <sifeij@gmail.com>
2023-10-16 17:55:19 -0700

    Create a custom pipe duration.pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import {
   CreateEventComponent,
   SessionListComponent,
   EventRouteActivator,
-  EventListResolver
+  EventListResolver,
+  DurationPipe
 } from './events/index';
 
 import { EventsAppComponent } from './events-app.component';
@@ -42,7 +43,8 @@ import { CollapsibleWellComponent } from './common/collapsible-well.component';
     NavBarComponent,
     CreateEventComponent,
     Error404Component,
-    CollapsibleWellComponent
+    CollapsibleWellComponent,
+    DurationPipe
   ],
   providers: [
     EventService, 
