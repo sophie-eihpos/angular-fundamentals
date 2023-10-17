398e300eb9ef64ca936e91a70642bb7f73c0ed10
Sophie Jiang <sifeij@gmail.com>
2023-10-16 19:06:02 -0700

    filter data on event detail page

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../shared";
 
 @Component({
     selector: 'session-list',
     templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
+    
     @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSession: ISession[] = [];
+    
+    ngOnChanges() {
+        if(this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter: string) {
+        if(filter === 'all') {
+            this.visibleSession = this.sessions;
+        } 
+        else {
+            this.visibleSession = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            });
+        };
+    }
 }
