288693a5f97612764e84fbe903aae7ba99d194ce
Sophie Jiang <sifeij@gmail.com>
2023-10-20 17:04:14 -0700

    add more test to the first test file to see if undefined and null are false

--- src/app/test.spec.ts
+++ src/app/test.spec.ts
@@ -1,5 +1,29 @@
-describe('first tests', () => {
-    it('should be true'), () => {
+describe('first tests spec file', () => {
+    it('should be true when true'), () => {
         expect(true).toBe(true);
+    }
+
+    it('should be false when false'), () => {
+        expect(false).toBe(false);
+    }
+
+    it('should be "true" when "true"'), () => {
+        expect('true').toBe('true');
+    }
+
+    it('should be "false" when "false"'), () => {
+        expect('false').toBe('false');
+    }
+
+    it('should be 2 when 1 + 1'), () => {
+        expect(1 + 1).toBe(2);
+    }
+
+    it('should be false when undefined'), () => {
+        expect(undefined).toBe(false);
+    }
+
+    it('should be false when null'), () => {
+        expect(null).toBe(false);
     }
 })
